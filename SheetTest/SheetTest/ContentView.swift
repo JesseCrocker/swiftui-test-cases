fee5f9494754988a5895cf6da674a9686f61089f
Jesse Crocker <jesse@datamongers.net>
2023-10-11 14:46:10 -0600

    Dont commit workaround in code

--- SheetTest/SheetTest/ContentView.swift
+++ SheetTest/SheetTest/ContentView.swift
@@ -49,7 +49,7 @@ struct Container1: View {
 }
 
 struct Container2: View {
-    @State var sheet2: Bool = false
+    @State var sheet2: Bool = true
     @Binding var container: ContentView.container
 
     var body: some View {
@@ -63,10 +63,6 @@ struct Container2: View {
                 container = .container1
             }
             Spacer()
-        }.onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                sheet2 = true
-            }
         }
         .padding()
         .sheet(isPresented: $sheet2) {
